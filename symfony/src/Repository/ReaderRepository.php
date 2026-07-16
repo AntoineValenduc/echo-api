78fdfef479930f039f66f6202f9eab7161337282
antoine <ant.valenduc@gmail.com>
2026-07-16 22:30:16 +0200

    feat/create_profile_entity: creation reader entity and repo

---
+++ symfony/src/Repository/ReaderRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Reader;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Reader>
+ */
+class ReaderRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Reader::class);
+    }
+
+    /**
+     * @return Reader Return a single reader with his ID
+     */
+    public function findOneById(int $id): ?Reader
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+//    /**
+//     * @return Reader[] Returns an array of Reader objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Reader
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
